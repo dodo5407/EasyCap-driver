e4026b10508900f61265e8c5e3782bce187af219
dodo5407 <a138949@gmail.com>
2017-07-12 01:51:36 +0800

    normal_module

--- smi2021/.smi2021.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/usr/src/linux-4.4/drivers/media/usb/smi2021/smi2021.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /usr/src/linux-4.4/drivers/media/usb/smi2021/smi2021.ko /usr/src/linux-4.4/drivers/media/usb/smi2021/smi2021.o /usr/src/linux-4.4/drivers/media/usb/smi2021/smi2021.mod.o
